83b0bf4022c0d98370757f45a85ba9644c604619
PKS-formido <116268671+PKS-formido@users.noreply.github.com>
2023-08-24 22:36:33 +0530

    Feat! : Completed user test cases

--- src/main/java/in/fssa/homebakery/util/ConnectionUtil.java
+++ src/main/java/in/fssa/homebakery/util/ConnectionUtil.java
@@ -6,8 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 //import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionUtil {
@@ -33,17 +31,9 @@ public class ConnectionUtil {
         String userName;
         String passWord;
 
-        if (System.getenv("CI") != null) {
             url = System.getenv("DATABASE_HOSTNAME");
             userName = System.getenv("DATABASE_USERNAME");
             passWord = System.getenv("DATABASE_PASSWORD");
-        } else {
-            Dotenv env = Dotenv.load();
-            url = env.get("DATABASE_HOSTNAME");
-            userName = env.get("DATABASE_USERNAME");
-            passWord = env.get("DATABASE_PASSWORD");
-        		
-        }
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
--- src/test/java/in/fssa/homebakery/TestCreateUser.java
+++ src/test/java/in/fssa/homebakery/TestCreateUser.java
@@ -20,7 +20,7 @@ public class TestCreateUser {
 		newUser.setFirstName("Formido");
 		newUser.setLastName("Boi");
 		newUser.setEmail("formido123@gmail.com");
-		newUser.setPassword("Aspirin2004");
+		newUser.setPassword("Aspirin@2004");
 		newUser.setPhoneNo(9345209293l);
 
 		assertDoesNotThrow(() -> {
@@ -48,11 +48,11 @@ public class TestCreateUser {
 	public void testCreateUserWithInvalidFirstName() {
 		UserService userService = new UserService();
 		User newUser = new User();
-		
+
 		newUser.setFirstName(null);
 		newUser.setLastName("Boi");
 		newUser.setEmail("formido123@gmail.com");
-		newUser.setPassword("Aspirin2004");
+		newUser.setPassword("Aspirin@2004");
 		newUser.setPhoneNo(9345209293l);
 
 		Exception exception = assertThrows(ValidationException.class, () -> {
@@ -64,37 +64,83 @@ public class TestCreateUser {
 
 		assertTrue(expectedMessage.equals(actualMessage));
 	}
-	
+
 	@Test
-	public void testCreateUserWithInvalidEmailPattern() {
+	public void testCreateUserWithEmptyFirstName() {
 		UserService userService = new UserService();
 		User newUser = new User();
-		
-		newUser.setFirstName("Formido");
+
+		newUser.setFirstName("");
 		newUser.setLastName("Boi");
-		newUser.setEmail("formgmail.com");
-		newUser.setPassword("Aspirin2004");
+		newUser.setEmail("formido123@gmail.com");
+		newUser.setPassword("Aspirin@2004");
 		newUser.setPhoneNo(9345209293l);
 
 		Exception exception = assertThrows(ValidationException.class, () -> {
 			userService.create(newUser);
 		});
 
-		String expectedMessage = "Email does not match the pattern";
+		String expectedMessage = "First Name cannot be null or empty";
 		String actualMessage = exception.getMessage();
 
 		assertTrue(expectedMessage.equals(actualMessage));
 	}
-	
+
+	@Test
+	public void testCreateUserWithInvalidFirstNamePattern() {
+		UserService userService = new UserService();
+		User newUser = new User();
+
+		newUser.setFirstName("Prave  1243");
+		newUser.setLastName("Boi");
+		newUser.setEmail("formido123@gmail.com");
+		newUser.setPassword("Aspirin@2004");
+		newUser.setPhoneNo(9345209293l);
+
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			userService.create(newUser);
+		});
+
+		String expectedMessage = "Name should only contain alphabets and be seprated by only one space";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.equals(actualMessage));
+	}
+
+	@Test
+	public void testCreateUserWithInvalidEmailPattern() {
+		UserService userService = new UserService();
+		User newUser = new User();
+
+		newUser.setFirstName("Formido");
+		newUser.setLastName("Boi");
+		newUser.setEmail("formgmail.com");
+		newUser.setPassword("Aspirin@2004");
+		newUser.setPhoneNo(9345209293l);
+
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			userService.create(newUser);
+		});
+
+
+		String expectedMessage = "Invalid email format. Please provide an email address with the following conditions:\n"
+				+ "- Should follow the standard email format (e.g., user@example.com)\n"
+				+ "- Should contain only letters, numbers, and special characters (@, .)\n"
+				+ "- Should have a valid domain name and top-level domain (e.g., .com, .org)";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.equals(actualMessage));
+	}
+
 	@Test
 	public void testCreateUserWithInvalidEmail() {
 		UserService userService = new UserService();
 		User newUser = new User();
-		
+
 		newUser.setFirstName("Formido");
 		newUser.setLastName("Boi");
 		newUser.setEmail(null);
-		newUser.setPassword("Aspirin2004");
+		newUser.setPassword("Aspirin@2004");
 		newUser.setPhoneNo(9345209293l);
 
 		Exception exception = assertThrows(ValidationException.class, () -> {
@@ -108,14 +154,35 @@ public class TestCreateUser {
 	}
 	
 	@Test
+	public void testCreateUserWithEmptyEmail() {
+		UserService userService = new UserService();
+		User newUser = new User();
+
+		newUser.setFirstName("Formido");
+		newUser.setLastName("Boi");
+		newUser.setEmail("");
+		newUser.setPassword("Aspirin@2004");
+		newUser.setPhoneNo(9345209293l);
+
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			userService.create(newUser);
+		});
+
+		String expectedMessage = "Email cannot be null or empty";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.equals(actualMessage));
+	}
+
+	@Test
 	public void testCreateUserWithInvalidPhoneNo() {
 		UserService userService = new UserService();
 		User newUser = new User();
-		
+
 		newUser.setFirstName("Formido");
 		newUser.setLastName("Boi");
 		newUser.setEmail("formidoboi123@gmail.com");
-		newUser.setPassword("Aspirin2004");
+		newUser.setPassword("Aspirin@2004");
 		newUser.setPhoneNo(123456789l);
 
 		Exception exception = assertThrows(ValidationException.class, () -> {
@@ -127,12 +194,12 @@ public class TestCreateUser {
 
 		assertTrue(expectedMessage.equals(actualMessage));
 	}
-	
+
 	@Test
 	public void testCreateUserWithInvalidPassword() {
 		UserService userService = new UserService();
 		User newUser = new User();
-		
+
 		newUser.setFirstName("Formido");
 		newUser.setLastName("Boi");
 		newUser.setEmail("formidoboi123@gmail.com");
@@ -144,6 +211,53 @@ public class TestCreateUser {
 		});
 
 		String expectedMessage = "Password cannot be null or empty";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.equals(actualMessage));
+	}
+	
+	@Test
+	public void testCreateUserWithEmptyPassword() {
+		UserService userService = new UserService();
+		User newUser = new User();
+
+		newUser.setFirstName("Formido");
+		newUser.setLastName("Boi");
+		newUser.setEmail("formidoboi123@gmail.com");
+		newUser.setPassword("");
+		newUser.setPhoneNo(6374111637l);
+
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			userService.create(newUser);
+		});
+
+		String expectedMessage = "Password cannot be null or empty";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(expectedMessage.equals(actualMessage));
+	}
+	
+	@Test
+	public void testCreateUserWithInvalidPasswordPattern() {
+		UserService userService = new UserService();
+		User newUser = new User();
+
+		newUser.setFirstName("Formido");
+		newUser.setLastName("Boi");
+		newUser.setEmail("formidoboi123@gmail.com");
+		newUser.setPassword("Praveen123");
+		newUser.setPhoneNo(6374111637l);
+
+		Exception exception = assertThrows(ValidationException.class, () -> {
+			userService.create(newUser);
+		});
+
+		String expectedMessage = "Invalid password format. Please provide a password with the following conditions:\n"
+				+ "- Should be at least 8 characters long\n"
+				+ "- Should contain at least one lowercase letter\n"
+				+ "- Should contain at least one uppercase letter\n"
+				+ "- Should contain at least one digit\n"
+				+ "- Should contain at least one special character (@, $, !, %, *, ?, &)";
 		String actualMessage = exception.getMessage();
 
 		assertTrue(expectedMessage.equals(actualMessage));
